4c5e44ca4156d6932962618fdd18899030602987
Chizaram <chukwumaobidivine87@gmail.com>
2024-10-03 21:09:39 -0700

    changes

--- src/view/LandingPage.jsx
+++ src/view/LandingPage.jsx
@@ -1,8 +1,36 @@
-const LandingPage=()=>{
+import { motion, AnimatePresence } from 'framer-motion';
+import LandingPageIcon from '/LandingPageIcon.svg';
 
-    return(
-        <>
-        </>
-    )
-}
-export default LandingPage
+const LandingPage = () => {
+  return (
+    <AnimatePresence>
+      <section className='landingPage'>
+        
+        {/* Landing page icon with animation from top */}
+        <motion.div
+          className="forLandingPageIcon"
+          initial={{ y: '-100%', opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          exit={{ y: '-100%', opacity: 0 }} 
+          transition={{ duration: 5, ease: 'easeInOut' }}
+        >
+          <img src={LandingPageIcon} alt="Edudesks Logo" />
+        </motion.div>
+
+        {/* Landing page logo with animation from right */}
+        <motion.div
+          className="forLandingPageLogo"
+          initial={{ x: '100%', opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          exit={{ x: '100%', opacity: 0 }}  
+          transition={{ duration: 10, ease: 'easeInOut' }}
+        >
+          <h1>Edu<span className='spanLogo'>desks</span>.</h1>
+        </motion.div>
+
+      </section>
+    </AnimatePresence>
+  );
+};
+
+export default LandingPage;

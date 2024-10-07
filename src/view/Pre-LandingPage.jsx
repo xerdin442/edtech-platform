ffda1a43abef192f65494a64c92954aa738f7231
Chizaram <chukwumaobidivine87@gmail.com>
2024-10-07 22:57:36 +0100

    changes

--- src/view/Pre-LandingPage.jsx
+++ src/view/Pre-LandingPage.jsx
@@ -19,7 +19,7 @@ const PreLandingPage = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setEduVisible(true);
-    }, 2700);
+    }, 2800);
     return () => clearTimeout(timer);
   }, []);
 
@@ -39,7 +39,7 @@ const PreLandingPage = () => {
                 key={index}
                 initial={{ opacity: 0, x: -10 }} 
                 animate={{ opacity: 1, x: 0 }} 
-                transition={{ delay: index * 0.1 }} 
+                transition={{ delay: index * 0.2 }} 
               >
                 {char}
               </motion.span>
